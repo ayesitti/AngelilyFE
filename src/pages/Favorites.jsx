6a0780b6fcf7949a9f3465aa4c6b0bc990f88251
Angelica Turbela <angelcturbela@gmail.com>
2023-08-08 10:22:24 +0200

    tuesday

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,6 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 function Favorites() {
+  const [hotels, setHotels] = useState(null)
+  const [userFavorites, setUserFavorites] = useState(null)
+
+  const fetchHotels = async () => 
+  setHotels((await axios.get("https://hooks.adaptable.app/hotels")).data)
+  const fetchFavorites = async () => {
+    try {
+      const response = await axios.get (
+        `https://hooks.adaptable.app/favorites?userId=${users.id}`
+      )
+      console.log(response.data) ;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  useEffect(() => {
+    fetchHotels()
+    fetchFavorites()
+  }, [])
+
+  async function addToFavorites(hotelId) {}
+  
+
   return (
     <div>Favorites</div>
   )
