9917431ac7f7c69ce10dda71e992d731d0d75f71
Angelica Turbela <angelcturbela@gmail.com>
2023-08-09 14:55:29 +0200

    icons

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import {MDeleteForever} from 'react-icons/md';
+import React from "react";
+import { RiDeleteBin2Line } from "react-icons/ri";
 
 function Note() {
   return (
-    <div className='note'>
-        <span>Hellooo Here is my firstnote!!! </span>
-        <div className='note-footer'>
-            <small>29/10/2023</small>
-            <MDeleteForever/>
-        </div>
+    <div className="note">
+      <span>Hellooo Here is my firstnote!!! </span>
+      <div className="note-footer">
+        <small>29/10/2023</small>
+        <RiDeleteBin2Line />
+      </div>
     </div>
-  )
+  );
 }
 
-export default Note
+export default Note;
