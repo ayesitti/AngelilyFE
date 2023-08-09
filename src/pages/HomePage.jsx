2dee546b02601be01e2741504404ada19eae5990
Angelica Turbela <angelcturbela@gmail.com>
2023-08-09 09:25:08 +0200

    cards

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,37 +1,34 @@
-import  {useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
 
 import { PiHeartBold } from "react-icons/pi";
 
-
 const API_URL = "https://hooks.adaptable.app/hotels";
 
-function HomePage({user}) {
-  
-  
+function HomePage({ user }) {
   const [hotels, setHotels] = useState([]);
-  const [page, setPage] = useState(1)
-  const [userFavorites, setUserFavorites] = useState([])
-  
+  const [page, setPage] = useState(1);
+  const [userFavorites, setUserFavorites] = useState([]);
+
   async function fetchAllHotels(page) {
     try {
-      const response = await axios.get(`${API_URL}?_page=${page}&_limit=9`);
+      const response = await axios.get(`${API_URL}?_page=${page}&_limit=12`);
       setHotels(response.data);
     } catch (error) {
       console.log(error);
     }
   }
-  
+
   async function addToFavorites(hotelId) {
     try {
       const newFavorite = {
         userId: user.id,
         hotelId: hotelId,
-      }
-      await axios.post("https://hooks.adaptable.app/favorites", newFavorite)
-      console.log('POST HOTEL')
-      fetchFavorites()
+      };
+      await axios.post("https://hooks.adaptable.app/favorites", newFavorite);
+      console.log("POST HOTEL");
+      fetchFavorites();
     } catch (error) {
       console.log(error.message);
     }
@@ -41,40 +38,38 @@ function HomePage({user}) {
   }, [page]);
 
   function handlePreviousButton() {
-    setPage(page - 1)
+    setPage(page - 1);
   }
 
   function handleNextButton() {
-    setPage(page + 1)
+    setPage(page + 1);
   }
 
   const fetchFavorites = async () => {
     try {
-      const response = await axios.get (
+      const response = await axios.get(
         `https://hooks.adaptable.app/favorites?userId=${user.id}`
-      )
-      console.log(response.data, "TEST")
-      setUserFavorites(response.data)
+      );
+      console.log(response.data, "TEST");
+      setUserFavorites(response.data);
     } catch (error) {
       console.log(error);
     }
-  }
+  };
 
   useEffect(() => {
-    fetchFavorites()
-  }, [])
-
+    fetchFavorites();
+  }, []);
 
   async function removeFavorites(favoriteId) {
-		try {
-			await axios.delete("https://hooks.adaptable.app/favorites/" + favoriteId)
-      console.log(favoriteId, 666666666)
-			fetchFavorites()
-		} catch (error) {
-			console.log(error.message)
-		}
-	}
-
+    try {
+      await axios.delete("https://hooks.adaptable.app/favorites/" + favoriteId);
+      console.log(favoriteId, 666666666);
+      fetchFavorites();
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
 
   if (!hotels) {
     return <div className="hotelLoading"> Loading..</div>;
@@ -84,22 +79,21 @@ function HomePage({user}) {
     <>
       <div className="homePage">
         {hotels.map((hotel) => {
-          	const isFav = userFavorites.find((fav) => fav.hotelId === hotel.id)
-            console.log(isFav, '5555555')
+          const isFav = userFavorites.find((fav) => fav.hotelId === hotel.id);
+          console.log(isFav, "5555555");
           return (
             <div key={hotel.title} className="hotelContainer">
               <img className="hotelImage" src={hotel.imgUrl} />
               <Link to={`/hotel/${hotel.title}`}>
                 <h2 className="hotel-name">{hotel.title}</h2>{" "}
               </Link>
-            <p className="hotel-address">{hotel.address}</p>
-            <p className="hotel-rating">Rating: {hotel.rating}</p>
-            {isFav 
-             ? ( 
-							<button onClick={() => removeFavorites(isFav.id)}>❤️</button>
-						) : (
-							<button onClick={() => addToFavorites(hotel.id) }>💔</button>
-						)}
+              <p className="hotel-address">{hotel.address}</p>
+              <p className="hotel-rating">Rating: {hotel.rating}</p>
+              {isFav ? (
+                <button onClick={() => removeFavorites(isFav.id)}>❤️</button>
+              ) : (
+                <button onClick={() => addToFavorites(hotel.id)}>💔</button>
+              )}
             </div>
           );
         })}
